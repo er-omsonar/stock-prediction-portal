d5d50304ec9d723e77f8f15cda747650d976828f
er-omsonar <er.omsonar@gmail.com>
2026-08-13 11:51:31 +0530

    registration page

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     "accounts",
     "api",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +130,7 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
+]
